1da2e599c23778d8f3eadc648c8056ba4bc3cc6a
bhambhanatanisha-web <bhambhanatanisha@gmail.com>
2026-01-05 10:49:47 +0530

    Add files via upload

---
+++ Time.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Timeconverter;
+
+int main()
+{
+    int totalSeconds, hours, minutes, seconds;
+
+    cout << "Enter total seconds: ";
+    cin >> totalSeconds;
+
+    if (totalSeconds >= 0)
+    {
+        hours = totalSeconds / 3600;
+        minutes = (totalSeconds % 3600) / 60;
+        seconds = totalSeconds % 60;
+
+        cout << "HH:MM:SS = "
+             << hours << ":" << minutes << ":" << seconds;
+    }
+}
